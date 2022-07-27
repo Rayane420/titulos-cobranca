73279789e3584e6aa83fc9c46d0d62654d148c2f
Rayane <rayanemaciel10@gmail.com>
2022-07-26 23:29:08 -0300

    iniciando projeto

---
+++ src/main/java/com/alga/cobranca/model/Titulo.java
@@ -0,0 +1,13 @@
+package com.alga.cobranca.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Titulo {
+    private Long codigo;
+    private String descrição;
+    private Date dataVencimento;
+    private BigDecimal valor;
+    private StatusTitulo status;
+
+}
